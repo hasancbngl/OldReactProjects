5073c4c2d2536b7b82161a2267609e8ef80bc7b8
Hasan Çobanoğlu <hasan.cobanoglu1@outlook.com.tr>
2021-02-15 17:11:34 +0300

    goal-manager

---
+++ goal-manager-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Airtable from 'airtable';
+
+const base = new Airtable({ apiKey:key}).base('id');
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
